e48582b744353c062e457ddc06f9f44de66ccd3f
DjakCode <djakcode@gmail.com>
2025-04-30 23:59:57 +0000

    adding GET and POST requests to create and fetch data from DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
 
 const PORT = 3001;
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
